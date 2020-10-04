38567c296f1cfbbc251d4835f1fc71f9c8918f8a
sevdaTan <sevdatanyildizi@gmail.com>
2020-10-04 10:16:05 -0400

    day9

--- src/test/java/com/cybertek/tests/day8_alerts_iframes_windows/AlertPractices.java
+++ src/test/java/com/cybertek/tests/day8_alerts_iframes_windows/AlertPractices.java
@@ -87,14 +87,6 @@ TC #1: Information alert practice
         }
 
 
-        /*
-        TC #3: Information alert practice
-        1. Open browser
-        2. Go to website: http://practice.cybertekschool.com/javascript_alerts
-        3. Click to “Click for JS Prompt” button
-        4. Send “hello” text to alert
-        5. Click to OK button from the alert
-        6. Verify “You entered: hello” text is displayed.
-        */
+
 
 }
