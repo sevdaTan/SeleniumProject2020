b08d16634ae6f7e35da3d4679050cf3162522bdd
sevdaTan <sevdatanyildizi@gmail.com>
2020-10-06 20:15:13 -0400

    day11

---
+++ src/test/java/com/cybertek/tests/day11_utilsReview_actions/Singleton.java
@@ -0,0 +1,26 @@
+package com.cybertek.tests.day11_utilsReview_actions;
+
+public class Singleton {
+
+    //Making the constructor private, so we limit creating objects from this class
+    private Singleton(){}
+
+    //Making the String private so that we can only reach this using getter method
+    public static String word;
+
+    //Creating a 'getter' method to allow users to reach private String above
+    public static String getWord(){
+
+        if (word == null){
+            System.out.println("First time call. Word object is null." +
+                    "Assigning value to it now!");
+            word = "something";
+        }else{
+            System.out.println("Word already has a value!");
+        }
+        return word;
+
+    }
+
+
+}
