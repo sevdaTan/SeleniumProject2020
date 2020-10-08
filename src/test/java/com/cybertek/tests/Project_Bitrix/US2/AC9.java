fcb63e3315b511b792fdbfe09db83a1034953eca
sevdaTan <68858980+sevdaTan@users.noreply.github.com>
2020-10-08 18:58:48 -0400

    Update AC9.java

--- src/test/java/com/cybertek/tests/Project_Bitrix/US2/AC9.java
+++ src/test/java/com/cybertek/tests/Project_Bitrix/US2/AC9.java
@@ -17,16 +17,15 @@ public class AC9 {
     //Username: helpdesk12@cybertekschool.com
     //Password: UserUser
 
-    WebDriver driver;
+   static WebDriver driver;
 
     @BeforeMethod
     public static void setupMethod() {
 
-        System.setProperty("WebDriverManager.chromedriver", "chromedriver");
-        WebDriverManager.chromedriver().setup();
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://login2.nextbasecrm.com");
 
         WebElement username = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
@@ -43,63 +42,93 @@ public class AC9 {
     @Test
     public void us2_ac9() throws InterruptedException {
 
+        Thread.sleep(2000);
 
         //It goes to Task module
        WebElement task = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
-       // WebElement task = driver.findElement(By.xpath("//span[.='Task']"));
-       // WebElement task = driver.findElement(By.partialLinkText("Tasks"));
-       // WebElement task = driver.findElement(By.id("feed-add-post-form-tab-tasks"));
+        // WebElement task = driver.findElement(By.xpath("//span[.='Task']"));
+        // WebElement task = driver.findElement(By.partialLinkText("Tasks"));
+        // WebElement task = driver.findElement(By.id("feed-add-post-form-tab-tasks"));
         task.click();
+        Thread.sleep(2000);
+
+
+        //It clicks on The Deadline Box
+
+        WebElement deadline = driver.findElement(By.xpath("//input[@class='task-options-inp']"));
+        deadline.click();
+
+        // choose a month
+        WebElement chooseMonth = driver.findElement(By.xpath("//a[@class='bx-calendar-top-month']"));
+        chooseMonth.click();
+
+        Thread.sleep(1000);
+        WebElement month = driver.findElement(By.xpath("//span[@class='bx-calendar-month bx-calendar-month-active']"));
+        month.click();
+
+        Thread.sleep(1000);
+
+        // choose a day
+        WebElement chooseDay = driver.findElement(By.xpath("//a[@data-date='1603065600000']"));
+        chooseDay.click();
+        Thread.sleep(1000);
+
+        // choose time
+       //WebElement chooseHour = driver.findElement(By.xpath("//input[@class='bx-calendar-form-input']"));
+        WebElement chooseHour = driver.findElement(By.xpath("//a[@data-dir='-1']"));
+        chooseHour.click();
+        chooseHour.click();
+        chooseHour.click();
+        chooseHour.click();
+        Thread.sleep(1000);
+
+        // choose Select
+        WebElement chooseSelect = driver.findElement(By.linkText("Select"));
+        chooseSelect.click();
+        Thread.sleep(1000);
+
+        // choose time planning
+        WebElement timePlanning = driver.findElement(By.xpath("//span[@data-target='date-plan']"));
+        timePlanning.click();
+        Thread.sleep(1000);
+
+        // click start task on
+        WebElement startTaskOn = driver.findElement(By.xpath("//span[@data-bx-id='dateplanmanager-start-date-plan']"));
+        startTaskOn.click();
         Thread.sleep(1000);
 
 
-        //It clicks on The New Task Button
-//        WebElement newTaskAdd = driver.findElement(By.id("tasks-buttonAdd"));
-//        newTaskAdd.click();
-//
-//        Thread.sleep(2000);
-//
-//        //It switch to the task iframe
-//        WebElement thingsToDoFrame = driver.findElement(By.xpath("//iframe[@class='side-panel-iframe']"));
-//
-//        driver.switchTo().frame(thingsToDoFrame);
+        WebElement calender = driver.findElement(By.xpath("//div[@class='bx-calendar-button-block']"));
+        calender.click();
+        Thread.sleep(1000);
 
-        //It types the task in the title of the task
+        //choose TimePlanning day
+        WebElement choosePlanningDay = driver.findElement(By.xpath("//a[@data-date='1603065600000']"));
+        choosePlanningDay.click();
+        Thread.sleep(1000);
 
+        // choose Select
+        WebElement chooseSelect2 = driver.findElement(By.linkText("Select"));
+        chooseSelect2.click();
+        Thread.sleep(1000);
 
-//      driver.getWindowHandles();
-//
-//      try {
-//          Thread.sleep(2000);
-//      } catch (InterruptedException e) {
-//          e.printStackTrace();
-//      }
-//        WebDriverWait wait = new WebDriverWait(driver, 20);
-//        wait.until(ExpectedConditions.elementToBeClickable(By.id("bx-b-link-task-form-lifefeed_task_form")));
-//
-//        WebElement linkButton = driver.findElement(By.id("bx-b-link-task-form-lifefeed_task_form"));
-//        linkButton.click();
-//
-//        WebElement linkTextField = driver.findElement(By.id("linklifefeed_task_form-href"));
-//        linkTextField.sendKeys("https://google.com");
-//
-//        WebElement linkTextName = driver.findElement(By.id("linklifefeed_task_form-text"));
-//        linkTextName.sendKeys("google link");
-//
-//        WebElement saveButton = driver.findElement(By.id("undefined"));
-//        saveButton.click();
-//
-//        WebElement taskTopic = driver.findElement(By.xpath("//input[@placeholder='Things to do']"));
-//        taskTopic.click();
-//        taskTopic.sendKeys("test12");
-//
-//        wait.until(ExpectedConditions.elementToBeClickable(By.id("blog-submit-button-save")));
-//        WebElement sendButton = driver.findElement(By.id("blog-submit-button-save"));
-//        sendButton.click();
-//
-//        WebElement createdTask = driver.findElement(By.xpath("//*[text()='test12']"));
-//        createdTask.click();
-   }
+       // choose duration
+        WebElement duration = driver.findElement(By.xpath("//input[@data-bx-id='dateplanmanager-duration']"));
+        duration.click();
+        Thread.sleep(1000);
+        duration.sendKeys("1");
+
+        // choose finishDate
+        WebElement finishDate = driver.findElement(By.xpath("//input[@data-bx-id='datepicker-display']"));
+        finishDate.click();
+        Thread.sleep(1000);
+
+        // choose close
+        WebElement chooseClose = driver.findElement(By.linkText("Close"));
+        chooseSelect2.click();
+        Thread.sleep(1000);
+
+    }
 
 
         @AfterMethod
@@ -110,6 +139,5 @@ public class AC9 {
 
 
     }
-
 
 
